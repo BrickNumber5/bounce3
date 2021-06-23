775a15140fba3030df8f0ab1707f565b9fa35c68
BrickNumber5 <79104486+BrickNumber5@users.noreply.github.com>
2021-06-22 19:58:57 -0600

    Add files via upload

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -10,15 +10,21 @@ function renderGame( ) {
   ctx.clearRect( 0, 0, w, h )
   ctx.translate( w / 2, h / 2 )
   ctx.scale( UNITSIZE, -UNITSIZE )
+  ctx.translate( -player.x, -player.y )
   let renderObject = renderLevelObjectType.bind( renderLevelObjectType, cnvs, ctx )
   
   renderObject( Segment )
   
   // Draw Player
-  ctx.fillStyle = COLOR.fgBlue
+  ctx.fillStyle = COLOR.player
   ctx.beginPath( )
-  ctx.arc( 0, 0, 1, 0, 2 * Math.PI )
+  ctx.arc( player.x, player.y, 1, 0, 2 * Math.PI )
   ctx.fill( )
+  
+  // Draw Debug
+  if ( SHOWDEBUGINFO ) {
+    currentLevel.objects.forEach( obj => obj.colliders ? obj.colliders.forEach( col => col.render( cnvs, ctx ) ) : null )
+  }
 }
 
 function renderLevelObjectType( cnvs, ctx, objType ) {
