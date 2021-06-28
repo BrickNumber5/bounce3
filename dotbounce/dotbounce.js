902e3ca9214b71985d6f4f794d24c3d4ed8cbb92
BrickNumber5 <79104486+BrickNumber5@users.noreply.github.com>
2021-06-27 22:22:18 -0600

    Add files via upload

--- dotbounce/dotbounce.js
+++ dotbounce/dotbounce.js
@@ -6,7 +6,155 @@
 const dotbounce = { }
 {
   dotbounce.parse = ( buffer, specialDictionary = undefined ) => {
+    let offset = 0, len = buffer.byteLength, view = new DataView( buffer )
+    function getObjectFromBuffer( ) {
+      insureAvailable( 1 )
+      let typeId = view.getUint8( offset )
+      offset++
+      if ( typeId === 0x01 ) {
+        return true
+      }
+      if ( typeId === 0x02 ) {
+        return false
+      }
+      if ( typeId === 0x0F ) {
+        return null
+      }
+      if ( typeId === 0x10 ) {
+        return
+      }
+      if ( typeId > 0x10 && typeId <= 0x18 ) {
+        return
+      }
+      if ( typeId === 0x20 ) {
+        return
+      }
+      if ( typeId > 0x20 && typeId <= 0x28 ) {
+        return
+      }
+      if ( typeId === 0x30 ) {
+        insureAvailable( 4 )
+        let obj = view.getFloat32( offset )
+        offset += 4
+        return obj
+      }
+      if ( typeId === 0x31 ) {
+        insureAvailable( 8 )
+        let obj = view.getFloat64( offset )
+        offset += 8
+        return obj
+      }
+      if ( typeId === 0x40 ) {
+        insureAvailable( 4 )
+        let length = view.getUint32( offset )
+        offset += 4
+        insureAvailable( length )
+        let decodingBuffer = new Uint8Array( length )
+        for ( let i = 0; i < length; i++ ) {
+          decodingBuffer[ i ] = view.getUint8( offset )
+          offset++
+        }
+        return new TextDecoder( ).decode( decodingBuffer )
+      }
+      if ( typeId === 0xA0 ) {
+        let obj = [ ]
+        while ( true ) {
+          insureAvailable( 1 )
+          if ( view.getUint8( offset ) === 0x00 ) {
+            offset++
+            break
+          } else {
+            obj.push( getObjectFromBuffer( ) )
+          }
+        }
+        return obj
+      }
+      if ( typeId === 0xB0 ) {
+        let obj = { }
+        while ( true ) {
+          insureAvailable( 1 )
+          let keyLen = view.getUint8( offset )
+          offset++
+          if ( keyLen === 0x00 ) {
+            break
+          } else {
+            insureAvailable( keyLen )
+            let decodingBuffer = new Uint8Array( keyLen )
+            for ( let i = 0; i < keyLen; i++ ) {
+              decodingBuffer[ i ] = view.getUint8( offset )
+              offset++
+            }
+            let key = new TextDecoder( ).decode( decodingBuffer )
+            obj[ key ] = getObjectFromBuffer( )
+          }
+        }
+        return obj
+      }
+      if ( typeId === 0xF0 ) {
+        insureAvailable( 1 )
+        let indexLen = view.getUint8( offset )
+        offset++
+        insureAvailable( indexLen )
+        let decodingBuffer = new Uint8Array( indexLen )
+        for ( let i = 0; i < indexLen; i++ ) {
+          decodingBuffer[ i ] = view.getUint8( offset )
+          offset++
+        }
+        let index = new TextDecoder( ).decode( decodingBuffer )
+        let values = [ ]
+        while ( true ) {
+          insureAvailable( 1 )
+          if ( view.getUint8( offset ) === 0x00 ) {
+            offset++
+            break
+          } else {
+            values.push( getObjectFromBuffer( ) )
+          }
+        }
+        return createSpecial( { index, values } )
+      }
+      if ( typeId >= 0x50 && typeId <= 0x9F ) {
+        let index = typeId - 0x50
+        let values = [ ]
+        while ( true ) {
+          insureAvailable( 1 )
+          if ( view.getUint8( offset ) === 0x00 ) {
+            offset++
+            break
+          } else {
+            values.push( getObjectFromBuffer( ) )
+          }
+        }
+        return createSpecial( { index, values } )
+      }
+      throw new Error( `0x${ typeId.toString( 16 ).padStart( 2, 0 ) } is not a valid type-id` )
+    }
     
+    function createSpecial( specifierObject ) {
+      if ( specialDictionary ) {
+        let localParser = specialDictionary.getParser( specifierObject.index )
+        if ( localParser ) {
+          return localParser( ...specifierObject.values )
+        }
+      }
+      let globalParser = dotbounce.globalSpecialDictionary.getParser( specifierObject.index )
+      if ( globalParser ) {
+        return globalParser( ...specifierObject.values )
+      }
+      return new dotbounce.UnknownSpecial( specifierObject.index, specifierObject.values )
+    }
+    
+    function insureAvailable( length, expected ) {
+      if ( offset + length > len ) {
+        throw new Error( "Early EOF" )
+      }
+    }
+    
+    let obj = getObjectFromBuffer( )
+    if ( offset !== len ) {
+      throw new Error( "Extra data past end of root data-item" )
+    }
+    return obj
   }
 
   dotbounce.encode = ( object, specialDictionary = undefined ) => {
@@ -135,7 +283,7 @@ const dotbounce = { }
         }
       }
       if ( typeof obj === "string" ) {
-        let encoded = new TextEncoder( ).encode( obj )
+        let encoded = new TextEncoder( ).encode( obj ).subarray( 0, 4294967295 )
         insureAvailable( 5 + encoded.length )
         view.setUint8( offset, 0x40 )
         offset++
@@ -181,6 +329,7 @@ const dotbounce = { }
     
     function addSpecialToBuffer( specialObject ) {
       if ( specialObject.integerIndex !== undefined ) {
+        if ( specialObject.integerIndex > 79 ) throw new Error( `A .bounce integer index may not be greater than 79, got: ${ specialObject.integerIndex }` )
         insureAvailable( 1 )
         view.setUint8( offset, 0x50 + specialObject.integerIndex )
         offset++
@@ -260,6 +409,23 @@ const dotbounce = { }
   dotbounce.SpecialDictionary = class extends Array {
     constructor( ...args ) {
       super( ...args )
+    }
+    
+    getParser( index ) {
+      // Returns a parser for the index, if one is in the dictionary, otherwise returns false
+      for ( let i = 0; i < this.length; i++ ) {
+        let indices = [ ]
+        if ( this[ i ].integerIndex ) indices.push( this[ i ].integerIndex )
+        if ( this[ i ].stringIndex ) indices.push( this[ i ].stringIndex )
+        if ( this[ i ].integerAliases ) indices.concat( this[ i ].integerAliases )
+        if ( this[ i ].stringAliases ) indices.concat( this[ i ].stringAliases )
+        for ( let j = 0; j < indices.length; j++ ) {
+          if ( indices[ j ] === index ) {
+            return this[ i ].parserFunction
+          }
+        }
+      }
+      return false
     }
     
     tryEncode( obj ) {
